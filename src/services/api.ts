56c69fee12f136d1dadbd7df169c17853b329950
Berényi Bence <berenyibence98@gmail.com>
2025-01-28 14:33:45 +0100

    temporary api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://petrix-backend-git-main-drinkmonsters-projects.vercel.app/api';
 
 export const getArticles = async (category?: string, page = 1) => {
   const params = new URLSearchParams({
